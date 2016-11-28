853751474252feb43e02e119407592ccf558b32d
rtfgvb74125 <105021056@live.asia.edu.tw>
2016-11-28 16:19:39 +0800

    1

--- src/ex/ex03.java
+++ src/ex/ex03.java
@@ -1,8 +1,9 @@
 ﻿package ex;
+import java.util.Scanner;
 /*
  * Topic: 讓使用者輸入一正整數 n，輸出 n 的所有正因數，並判斷該整數是否為質數
  * Date: 2016/11/28
- * Author: 1050210XX 周永振老師
+ * Author: 105021056 王家恩
  */
 
 public class ex03 {
@@ -10,6 +11,18 @@ public class ex03 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int sum = 0;
+		for(int i = 1;i<n;i++){
+			 if(n/i==0){
+				 sum = sum+(n%i);
+			 }else{
+			
+			
+			System.out.print(i+" ");
+		}
 	}
 
+}
 }
